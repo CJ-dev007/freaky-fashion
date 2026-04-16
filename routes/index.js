181b5b51ad821f728e07d2cb64a0e6e6e385da27
Chatie Jonsén <c.daniels@hotmail.com>
2026-04-16 20:59:11 +0200

    Homepage refresh: grid, features, spots, images & logo update

--- routes/index.js
+++ routes/index.js
@@ -7,17 +7,30 @@ const db = require('../data/db');
 // GET http://localhost:3000/
 router.get('/', function(req, res, next) {
  
-  const products = [
-        { id: 1, name: "Lorem Ipsum dolor", image: "https://placehold.co/300x200" },
-        { id: 2, name: "Lorem Ipsum dolor", image: "https://placehold.co/300x200" },
-        { id: 3, name: "Lorem Ipsum dolor", image: "https://placehold.co/300x200" }
+  const spots = [
+        { id: 1, name: "Detaljerna som Gör Det", image: "images/spots6.avif" },
+        { id: 2, name: "Livet är bättre i flip-flops", image: "images/spots9.avif" },
+        { id: 3, name: "Mest Populära Just Nu", image: "images/spots4.avif" }
     ];
 
+  const popularProducts = [
+    { id: 1, name: "Svart T-shirt", price: 199, brand: "Levis", image: "images/tshirt1.avif", isNew: true, isFavorite: false },
+    { id: 2, name: "Svart T-shirt", price: 199, brand: "Levis", image: "images/tshirt2.avif", isNew: true, isFavorite: false },
+    { id: 3, name: "Svart T-shirt", price: 199, brand: "Levis", image: "images/tshirt3.avif", isNew: true, isFavorite: false },
+    { id: 4, name: "Svart T-shirt", price: 199, brand: "Levis", image: "images/tshirt4.avif", isNew: true, isFavorite: false },
+    { id: 5, name: "Svart T-shirt", price: 199, brand: "Levis", image: "images/tshirt5.avif", isNew: true, isFavorite: false },
+    { id: 6, name: "Svart T-shirt", price: 199, brand: "Levis", image: "images/tshirt6.avif", isNew: true, isFavorite: false },
+    { id: 7, name: "Svart T-shirt", price: 199, brand: "Levis", image: "images/tshirt7.avif", isNew: true, isFavorite: false },
+    { id: 8, name: "Svart T-shirt", price: 199, brand: "Levis", image: "images/tshirt8.avif", isNew: true, isFavorite: false }
+  ]
+
   res.render('index', {
-    title: 'FreakyFashion',
-    heroTitle: "Lorem ipsum dolor", 
-    heroText: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
-    spots: products
+    title: 'Freaky Fashion',
+    heroTitle: "Wild Spirit, Freaky Soul", 
+    heroText: "Sommarens mest eftertraktade prints är här. Maxa din festival-look med grafiska tees som sticker ut i mängden.",
+    heroImage: "images/hero.avif",
+    spots: spots,
+    products: popularProducts
   });
 });
 
